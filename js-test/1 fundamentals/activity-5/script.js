cb5f1c7ad8775209569aa32f46287d754b2740a8
Vittela <vittela.code@gmail.com>
2025-11-12 23:29:48 -0300

    Activity 5 and 6 addeds

---
+++ js-test/1 fundamentals/activity-5/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let response = prompt("Write a number");
+
+if (response > 0){
+    alert(1)
+} else if (response === 0) {
+    alert(0)
+} else {
+    alert(-1)
+}
---
+++ js-test/1 fundamentals/activity-6/script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let age = prompt("Write your age!")
+
+if (age >= 14 && age <= 90) {
+    alert("You have something between 14 and 90!")
+} else {
+    alert("Or you are a child, or you are really old.")
+}
